6bc10c95a22c28858d00caba0206b82fda626020
Abo2507 <dauka20015@gmail.com>
2026-02-13 16:25:01 +0500

    Add caching to HouseService methods
    Implemented caching for house retrieval methods to improve performance. Evicted cache on house creation, update, and deletion.

--- src/main/java/kz/hogwarts/service/HouseService.java
+++ src/main/java/kz/hogwarts/service/HouseService.java
@@ -1,6 +1,7 @@
 package kz.hogwarts.service;
 
 import kz.hogwarts.model.House;
+import kz.hogwarts.patterns.singleton.CacheService;
 import kz.hogwarts.repository.HouseRepository;
 import kz.hogwarts.utils.ValidationUtils;
 import org.springframework.stereotype.Service;
@@ -10,19 +11,46 @@ import java.util.List;
 @Service
 public class HouseService {
 
+    private static final String CACHE_KEY_ALL_HOUSES = "houses:all";
+    private static final String CACHE_PREFIX_HOUSES = "houses:";
+
     private final HouseRepository houseRepository;
+    private final CacheService cache = CacheService.getInstance();
 
     public HouseService(HouseRepository houseRepository) {
         this.houseRepository = houseRepository;
     }
 
+    /**
+     * BONUS TASK: Cached method - houses rarely change
+     */
+    @SuppressWarnings("unchecked")
     public List<House> getAllHouses() {
-        return houseRepository.findAll();
+        // Try cache first
+        Object cached = cache.get(CACHE_KEY_ALL_HOUSES);
+        if (cached != null) {
+            return (List<House>) cached;
+        }
+
+        List<House> houses = houseRepository.findAll();
+        cache.put(CACHE_KEY_ALL_HOUSES, houses);
+
+        return houses;
     }
 
     public House getHouseById(Integer id) {
         ValidationUtils.validatePositive(id, "House ID");
-        return houseRepository.findById(id);
+
+        String cacheKey = CACHE_PREFIX_HOUSES + id;
+        Object cached = cache.get(cacheKey);
+        if (cached != null) {
+            return (House) cached;
+        }
+
+        House house = houseRepository.findById(id);
+        cache.put(cacheKey, house);
+
+        return house;
     }
 
     public House createHouse(House house) {
@@ -33,20 +61,29 @@ public class HouseService {
             house.setPoints(0);
         }
 
-        return houseRepository.save(house);
-    }
+        House created = houseRepository.save(house);
 
+        cache.evictByPrefix(CACHE_PREFIX_HOUSES);
+
+        return created;
+    }
 
     public House updateHouse(Integer id, House house) {
         ValidationUtils.validatePositive(id, "House ID");
         ValidationUtils.validateNotEmpty(house.getName(), "House name");
         ValidationUtils.validateNotEmpty(house.getFounder(), "Founder");
 
-        return houseRepository.update(id, house);
+        House updated = houseRepository.update(id, house);
+
+        cache.evictByPrefix(CACHE_PREFIX_HOUSES);
+
+        return updated;
     }
 
     public void deleteHouse(Integer id) {
         ValidationUtils.validatePositive(id, "House ID");
         houseRepository.delete(id);
+
+        cache.evictByPrefix(CACHE_PREFIX_HOUSES);
     }
 }
